3d8e63d2a14e506a8a9c0c316bbfe2a49378965b
Shane <143809335+ShaneJ-Ai@users.noreply.github.com>
2026-01-04 17:23:58 +0100

    feat: add prisma sqlite setup and user model

--- src/prisma.js
+++ src/prisma.js
@@ -1,2 +1,5 @@
 // SPDX-License-Identifier: LicenseRef-LICENSE
 //(c) Copyright 2026 Shane Flaten (CodingAce), all rights reserved.
+
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
